a0c4f787629ebad33aa821c4c7cadc638b22b2a5
RonalldoTeixeira <ronalldo.tb@gmail.com>
2023-06-09 20:00:08 -0300

    tests

---
+++ tests/business/getUsers.test.ts
@@ -0,0 +1,35 @@
+import {UserBusiness} from "../../src/business/UserBusiness"
+import { GetUsersSchema } from "../../src/dtos/user/getUsers.dto"
+import {HashManagerMock} from "../mocks/HashManagerMock"
+import {IdGeneratorMock} from "../mocks/IdGeneratorMock"
+import {TokenManagerMock} from "../mocks/TokenManagerMock"
+import {UserDatabaseMock} from "../mocks/UserDatabaseMock"
+import {USER_ROLES} from "../../src/models/User"
+
+describe("Testando método getUsers", ()=>{
+    const userBusiness = new UserBusiness(
+        new UserDatabaseMock(),
+        new IdGeneratorMock(),
+        new TokenManagerMock(),
+        new HashManagerMock()
+    )
+
+    test("deve retornar uma lista de users", async()=>{
+
+        const input = GetUsersSchema.parse({
+            token: "token-mock-astrodev"
+        })
+
+        const output = await userBusiness.getUsers(input)
+        expect(output).toHaveLength(2)
+        expect(output).toContainEqual({
+            id: "id-mock-astrodev",
+            name: "Astrodev",
+            email: "astrodev@email.com",
+            createdAt: expect.any(String),
+            role: USER_ROLES.ADMIN
+        })
+
+    })
+
+})
